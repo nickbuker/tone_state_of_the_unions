b997d4ddcefd3b1902555ca7951dd4f1e87cc569
nickbuker <nickbuker@gmail.com>
2017-03-30 16:56:37 -0700

    add python plotting functions

--- src/plotting.py
+++ src/plotting.py
@@ -1,9 +1,38 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def individual_tones(series, color, y_lab, figsize=(14, 8)):
+def individual_tones(df, tone, colors, title, y_lab, figsize=(12, 6)):
     plt.figure(figsize=(figsize))
-    plt.plot(series, color=color)
+    plt.plot(df[tone], color=colors[tone], marker='o', label=y_lab)
     plt.title(title, fontsize=18, fontweight='bold')
     plt.xlabel("Year", fontsize=14, fontweight='bold')
     plt.ylabel(y_lab, fontsize=14, fontweight='bold')
+    plt.ylim(0, 0.8)
+    plt.axhline(np.mean(df[tone]), color='k', linestyle=':', label='Mean')
+    plt.legend(fontsize=14)
+
+
+def multiple_tones(df, tone_labels, colors, title,
+                   figsize=(12, 6), y_lim=(-0.1, 1)):
+    plt.figure(figsize=(figsize))
+    for i, tone in enumerate(tone_labels):
+        plt.plot(df[tone], color=colors[tone], marker='o', label=tone)
+    plt.title(title, fontsize=18, fontweight='bold')
+    plt.xlabel("Year", fontsize=14, fontweight='bold')
+    plt.ylabel("Score", fontsize=14, fontweight='bold')
+    plt.ylim(y_lim)
+    plt.legend(fontsize=14)
+
+
+def stacked_bar(df, colors, figsize=(12, 8), y_lim=(0, 2.5)):
+    plt.figure(figsize=figsize)
+    bottom = 0
+    for i, col in enumerate(df.columns):
+        plt.bar(df.index, df[col], bottom=bottom, alpha=0.8,
+                color=colors[col], label=col)
+        bottom += df[col]
+    plt.legend(fontsize=10)
+    plt.xlabel('Year', fontsize=16)
+    plt.ylabel('Tone Score', fontsize=16)
+    plt.title('Stacked Bar Graph', fontsize=18, fontweight='bold')
