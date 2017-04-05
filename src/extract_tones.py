eac9cc28f9c18a1577b7628a435a2f2b73cbf3b7
nickbuker <nickbuker@gmail.com>
2017-04-05 14:06:10 -0700

    add tone extraction functions

---
+++ src/extract_tones.py
@@ -0,0 +1,18 @@
+def anger(element):
+    return element['Anger']
+
+
+def disgust(element):
+    return element['Disgust']
+
+
+def sadness(element):
+    return element['Sadness']
+
+
+def joy(element):
+    return element['Joy']
+
+
+def fear(element):
+    return element['Fear']
